8bb45c9a1565a2620fb50e3906e2b63c65f59c8d
Murat <renja.dizz@gmail.com>
2023-10-29 03:00:29 +0600

    add open/close menu feature

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
 import './index.html';
 import './index.scss';
+import * as menu from "./modules/menu.js";
+
+// menu
+let menuShowButton = document.querySelector("#btn-menu-show");
+menuShowButton.onclick = menu.menuShow;
+let menuCloseButton = document.querySelector("#btn-menu-close");
+menuCloseButton.onclick = menu.menuClose;
+//
+
+
---
+++ src/modules/menu.js
@@ -0,0 +1,18 @@
+let menuPanel = document.querySelector(".menu");
+let mainContent = document.querySelector(".main-content");
+
+export function menuShow() {
+    menuPanel.style.cssText = "margin-left: 0";
+    menuPanel.style.height = "auto";
+    menuPanel.style.boxShadow = `2rem 0 3rem #F2F2F2`;
+    mainContent.style.filter = "blur(1rem)";
+    mainContent.addEventListener("mousedown", menuClose);
+}
+
+export function menuClose(event) {
+    event.stopPropagation();
+    menuPanel.style.cssText = "margin-left: -320px";
+    menuPanel.style.boxShadow = `none`;
+    mainContent.style.filter = "none";
+    mainContent.removeEventListener("mousedown", menuClose);
+}
