102af72ad8b72b70ac170fde1f877a85f8da4024
Murat <renja.dizz@gmail.com>
2023-10-29 18:15:03 +0600

    add transition to side menu

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -3,7 +3,6 @@ let mainContent = document.querySelector(".main-content");
 
 export function menuShow() {
     menuPanel.style.cssText = "margin-left: 0";
-    menuPanel.style.height = "auto";
     menuPanel.style.boxShadow = `2rem 0 3rem #F2F2F2`;
     mainContent.style.filter = "blur(1rem)";
     mainContent.addEventListener("mousedown", menuClose);
